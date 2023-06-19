e24d02cc9213533b3c953c3d7778ce544dd53eb1
willkrakow <willkrakow@gmail.com>
2023-06-18 21:45:16 -0400

    Remove old references.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,37 +31,38 @@ export default function Home() {
 const sections = [
   {
     textColumn: {
-      label: "About me",
-      title: "Hi there!",
-      description: "My name's Will. I'm a full-stack developer from Hillsborough, NC, with a background in mathematics and an affection for intuitive, original designs. In my spare time I like to hike, ride bikes, and play around with new Javascript libraries. I am seeking full time or contract employment. Keep reading to learn more about me and my work.",
+      label: 'About me',
+      title: 'Hi there!',
+      description:
+        "My name's Will. I'm a software engineer with a background in applied mathematics. These days I work mostly with TypeScript, React, Node, Docker, and AWS.",
     },
     cta: {
-      label: "View resume",
-      link: "/resume",
-      alt: "Resume | William Krakow"
+      label: 'View resume',
+      link: '/resume',
+      alt: 'Resume | William Krakow',
     },
     right: SocialLinks,
   },
   {
     textColumn: {
-      label: "Projects",
+      label: 'Projects',
       title: "Things I've built",
-      description: "I primarily work on the frontend, and have recently taken a liking to the JAMstack architecture. I've also worked extensively in Node.js on projects large and small, for both complete servers and serverless functions. Beyond web dev, I like using Python for web scraping, data analytics, and machine learning via Tensorflow/Keras.",
-      
+      description:
+        'Everything from frontend design concepts to fullstack applications and security vulnerability research.',
     },
     cta: {
-        label: "View more projects",
-        link: "/projects",
-        alt: "Development and Design Projects | William Krakow"
+      label: 'View more projects',
+      link: '/projects',
+      alt: 'Development and Design Projects | William Krakow',
     },
     right: ProjectList,
   },
   {
     textColumn: {
-      label: "Skillset",
-      title: "Tech I love",
+      label: 'Skillset',
+      title: 'Tech I love',
       description:
-        "These are the libraries, frameworks, languages and tools that I like to use when building personal projects. I'm always open to adding to my toolbox and embracing new technologies.",
+        "The libraries, frameworks, languages and tools that I'd choose for a new project, as of 2023.",
     },
     right: TechIcons,
   },
